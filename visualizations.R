fb8754c4a755db525436889d626e70597a5c9229
rovle <lovre.pesut@gmail.com>
2022-06-01 14:55:58 +0200

    finish creating visualizations in R

--- visualizations.R
+++ visualizations.R
@@ -7,37 +7,47 @@ library("ggrepel")
 library("ggthemes")
 theme_set(theme_bw())
 
-dat <- read.csv("distribution_samples.csv",
+dat <- read.csv("results/distribution_samples.csv",
                 header= TRUE,
                 stringsAsFactors = FALSE,
                 sep = ",")
 
 dat$clas <- as.character(dat$clas)
 ggplot(dat, aes(x=x, y=y, color=clas)) +
-  geom_point(size=2) +
+  geom_point(size=1) +
   facet_wrap(~ type, ncol=3) +
   scale_color_discrete(name="Class") +
   labs(title="", x="", y="") +
   theme(legend.position = "none",
-        panel.border = element_rect(colour="#fe5185", size=0.8),
-          strip.background = element_rect(colour="blue",
+        panel.border = element_rect(colour="black", size=0.8),
+          strip.background = element_rect(colour="#fe5185",
                                           fill="white", size=0.8),
-        axis.text.x=element_text(colour="#fe5185"),
-        axis.text.y=element_text(colour="#fe5185"),
-        axis.ticks = element_line(colour = '#fe5185', size = 0.8, linetype = 'dashed')
+        axis.text.x=element_text(colour="black"),
+        axis.text.y=element_text(colour="black"),
+        axis.ticks = element_line(colour = 'black', size = 0.8, linetype = 'dashed')
         )
 
-dat2 <- read.csv('differences.csv',
+ggsave('plots/distribucije.png', width = 6, height = 6,
+                        dpi = 200, units = "in")
+
+dat2 <- read.csv('results/differences.csv',
                  header=TRUE,
                  sep=",")
 dat2 %>% pivot_longer(-X) %>%
   ggplot(aes(x=name,y=value,fill=name))+
-  labs(title="", x="", y="Percentage better than KNN") +
+  labs(title="", x="", y="Accuracy compared to kNN's") +
   geom_boxplot(alpha=0.8)+ 
   geom_jitter(color="black", size=0.4, alpha=0.8)+
-scale_y_continuous(labels = scales::percent)
+scale_y_continuous(labels = scales::percent) +
+  theme(legend.position=c(0.2, 0.18),
+        legend.title=element_blank(),
+        legend.box.background = element_rect(colour = "purple", size=1),
+        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'))
 
-reg1 <- read.csv('linear_model_1_input_15.csv',
+ggsave('plots/razlike.jpg', width = 6, height = 6,
+       dpi = 200, units = "in")
+
+reg1 <- read.csv('results/linear_model_1_input_15.csv',
                  header=TRUE,
                  sep=",")
 
@@ -48,9 +58,19 @@ ggplot(reg1, aes(x=x, y=y, color=from)) +
   scale_color_hue(name = "",
                   labels = c("GPT-3 predictions",
                              "Training points",
-                             "True regression fn."))
+                             "True regression fn.")) +
+  theme(legend.position=c(0.8, 0.8),
+        legend.title=element_blank(),
+        legend.box.background = element_rect(colour = "purple", size=1),
+        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank())
 
-reg2 <- read.csv('linear_model_2_input_15.csv',
+ggsave('plots/reg1.jpg', width = 6, height = 6,
+       dpi = 200, units = "in")
+
+
+reg2 <- read.csv('results/linear_model_2_input_15.csv',
                  header=TRUE,
                  sep=",")
 
@@ -66,10 +86,14 @@ ggplot(reg2, aes(x=x, y=y, color=from)) +
   theme(legend.position=c(0.8, 0.2),
         legend.title=element_blank(),
         legend.box.background = element_rect(colour = "purple", size=1),
-        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'))
+        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank())
 
+ggsave('plots/reg2.jpg', width = 6, height = 6,
+       dpi = 200, units = "in")
 
-reg3 <- read.csv('quadratic_model_2_input_25_smaller_variance.csv',
+reg3 <- read.csv('results/quadratic_model_2_input_25_smaller_variance.csv',
                  header=TRUE,
                  sep=",")
 
@@ -82,12 +106,18 @@ ggplot(reg3, aes(x=x, y=y, color=from)) +
                              "True regression fn.")) +
   geom_vline(xintercept=24, linetype='dotted', col = 'purple', label="Training bound") +
   geom_vline(xintercept=75, linetype='dotted', col = 'purple') +
-  theme(legend.position=c(0.8, 0.2),
+  theme(legend.position=c(0.2, 0.8),
         legend.title=element_blank(),
         legend.box.background = element_rect(colour = "purple", size=1),
-        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'))
+        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank())
 
-reg4 <- read.csv('quadratic_model_1_input_25.csv',
+ggsave('plots/reg3.jpg', width = 6, height = 6,
+       dpi = 200, units = "in")
+
+
+reg4 <- read.csv('results/quadratic_model_1_input_25.csv',
                  header=TRUE,
                  sep=",")
 
@@ -104,5 +134,9 @@ ggplot(reg4, aes(x=x, y=y, color=from)) +
   theme(legend.position=c(0.2, 0.2),
         legend.title=element_blank(),
         legend.box.background = element_rect(colour = "purple", size=1),
-        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'))
+        legend.margin=margin(t=-0.18,l=0.05,b=0.0,r=0.05, unit='cm'),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank())
 
+ggsave('plots/reg4.jpg', width = 6, height = 6,
+       dpi = 200, units = "in")
